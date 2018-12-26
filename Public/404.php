924eb164fd9a9fc3cde03c03cc3ce375ddcf547d
Nguyễn Hồng Kỳ <1660296@student.hcmus.edu.vn>
2018-12-26 14:25:05 +0700

    Create 404 page

---
+++ Public/404.php
@@ -0,0 +1,32 @@
+<?php 
+require_once('load.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title>Lỗi 404</title>
+
+	<link href="<?php asset('css/ggFont1') ?>" rel="stylesheet">
+	<link href="<?php asset('css/ggFont1') ?>" rel="stylesheet">
+
+
+	<link type="text/css" rel="stylesheet" href="<?php asset('css/404.css') ?>">
+</head>
+
+<body>
+	<div id="notfound">
+		<div class="notfound">
+			<div class="notfound-404">
+				<h3>Có vấn đề! Trang không thể tìm thấy</h3>
+				<h1><span>4</span><span>0</span><span>4</span></h1>
+			</div>
+			<h2>Chúng tôi rất làm tiếc,<br> trang bạn yêu cầu không tìm thấy</h2>
+		</div>
+	</div>
+
+</body>
+</html>
