75ffb9345bb5f5e19d0ff04ffdec8f573d2acd6a
Nguyen Huu Khanh <nhkhanh861998@gmail.com>
2018-12-27 03:33:54 +0700

    upload het fronend admin

---
+++ View/layout/process.php
@@ -0,0 +1,28 @@
+<img src="images/clothes2.jpg" class=img_special alt="">
+
+    <section class="Content">
+      
+
+        <h2 class="Content-ten">Trạng thái đơn hàng</h2>
+
+        <div class="stepwizard Content-step">
+            <div class="stepwizard-row">
+                <div class="stepwizard-step">
+                    <button type="button" class="btn btn-default btn-circle">1</button>
+                    <p>Chờ kiểm duyệt</p>
+                </div>
+                <div class="stepwizard-step">
+                    <button type="button" class="btn btn-primary btn-circle">2</button>
+                    <p>Đang vận chuyển</p>
+                </div>
+                <div class="stepwizard-step">
+                    <button type="button" class="btn btn-default btn-circle" disabled="disabled">3</button>
+                    <p>Thanh toán</p>
+                </div> 
+            </div>
+        </div>
+   
+
+
+   
+    </section>
