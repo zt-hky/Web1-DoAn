0b27c17a2b9e6a46cb552b2108d623beea008b9a
Nguyễn Hồng Kỳ <1660296@student.hcmus.edu.vn>
2018-12-27 16:19:36 +0700

    Update sanphamDAO.php

--- DAO/sanphamDAO.php
+++ DAO/sanphamDAO.php
@@ -59,7 +59,7 @@ class SanPhamDAO extends DB
 
     public function getByID($id)
     {
-        $sql = "SELECT idSanPham, TenSP, Gia, SLXem,SLBan,MoTa,XuatXu, LinkURL, idLoaiSanPham, idNSX, Time, Deleted FROM SANPHAM WHERE idSanPham = $id";
+        $sql = "SELECT idSanPham, TenSP, Gia, SLXem,SLBan,MoTa,XuatXu, LinkURL, idLoaiSanPham, idNSX, Img Time, Deleted FROM SANPHAM WHERE idSanPham = $id";
         $result = $this->ExcuteQuery($sql);
         $row = mysqli_fetch_array($result);
         $SanPham = new DTO\SanPham();
